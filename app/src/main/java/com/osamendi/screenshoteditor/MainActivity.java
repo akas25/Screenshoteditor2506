f6aa8759cdcc480ad7530abbedfc8513bf8531f0
lolbd307-png <lolbd307@gmail.com>
2026-03-06 23:47:07 +0600

    Initial Android project with GitHub Actions APK build

---
+++ app/src/main/java/com/osamendi/screenshoteditor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.osamendi.screenshoteditor;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView statusText = findViewById(R.id.statusText);
+        statusText.setText("App build success. Next step: screenshot OCR editor logic add.");
+    }
+}
